d3257f76e4cb371a19eef37fd2f25fb9ca18d693
vladimirov535 <39128149+vladimirov535@users.noreply.github.com>
2023-06-30 17:11:39 +0300

    Add files via upload

--- alert.js
+++ alert.js
@@ -1,3 +1,44 @@
-let age = prompt('Твео имя?');
+// let login = prompt('login');
 
-alert(`твое имя ${age} `); // Тебе 100 лет!
+// let message = (login == 'Сотрудник') ? 'Привет' :
+//   (login == 'Директор') ? 'Здравствуйте' :
+//   (login == '') ? 'Нет логина' :
+//   '';
+//   alert(login);
+
+
+
+// let hour =  prompt('Время');
+
+// let m1 =(hour < 10 || hour > 18)? 'Закрыто':
+//  (hour > 10 || hour < 18)? 'Открыто':
+//  (hour == '')? 'Ошибка':
+//    '';
+//   alert(m1);
+//   console.log(hour);
+
+// let hour =  prompt('Цифра');
+// if (hour == 5) {
+//   let message =('Это пятерка');
+// } else {
+//   let message =('Это не пятерка');
+// }
+// alert(message);
+
+let time = prompt('Время', '');
+
+let message;
+
+if (time == 'Number.isInteger') {
+  message = 'Ошибка';
+} else  {
+  time = Number(time)
+
+  if (time > 10 || time < 18) {
+    message = 'Открыто';
+  } else {
+    message = 'Закрыто';
+  }
+}
+
+alert(message);
