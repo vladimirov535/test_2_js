1b7920c8d31bd7adef92d4760ddc738cd4dc0c5b
vladimirov535 <39128149+vladimirov535@users.noreply.github.com>
2023-07-05 10:24:36 +0300

    finish commit 05.07.2023

--- alert.js
+++ alert.js
@@ -1,29 +1,13 @@
-// let time = prompt('Время', '');
 
-// let message;
-
-// time = Number(time)
-// if (isNaN(time) || time == " " ) {
-//   message = 'Ошибка';
-// } else {
-
-//   if (time > 10 && time < 18) {
-//     message = 'Открыто';
-//   } else {
-//     message = 'Закрыто';
-//   }
-// }
-
-// alert(message);
 
 let timeString = prompt("Время", "");
 
 let message;
 let time = parseInt(timeString);
-if ((time < 0 || time > 23) || isNaN(time)) {
+if (isNaN(time) || time < 0 || time > 23  ) {
   message = "ошибка";
 } else {
-  if (time => 10 && time <= 18) {
+  if (time >= 10 && time <= 18) {
     message = "Открыто";
   } else {
     message = "Закрыто";
@@ -32,53 +16,3 @@ if ((time < 0 || time > 23) || isNaN(time)) {
 
 alert(message);
 
-// let time = prompt('Время', '');
-
-// let message;
-
-// time = Number(time);
-// if (typeof time != 'number'
-// ) {
-//   message = 'Ошибка';
-// } else {
-
-//   if (time > 10 && time < 18) {
-//     message = 'Открыто';
-//   } else {
-//     message = 'Закрыто';
-//   }
-// }
-
-// alert(message);
-
-// let time = prompt('Время', '');
-
-// let message;
-
-// time = Number(time);
-
-// if (Number.isNaN(Number(time))
-// ) {
-//   message = 'Ошибка';
-// } else {
-
-//   if (time > 10 && time < 18) {
-//     message = 'Открыто';
-//   } else {
-//     message = 'Закрыто';
-//   }
-// }
-
-// alert(message);
-
-// let userName = prompt("Время?", '');
-// let message;
-
-//   if (userName >= 10 $$ userName <= 18) {
-//     message= 'Открыто' ;
-//   } else if (!(userName >= 10 $$ userName <= 18)) {
-//     message= 'Закрыто' ;
-//   } else {
-//     message= 'Ошибка' ;
-//   }
-//   alert(message);
